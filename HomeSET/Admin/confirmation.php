dd4ed4b49c759a8e5d1a07370eca07ab155bcad9
jld5692 <jerome.ledorze@gmail.com>
2021-11-14 18:30:55 +0100

    Hashage password

---
+++ HomeSET/Admin/confirmation.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="../style.css" media="screen" type="text/css" />
+    </head>
+    <body style='background:#fff;'>
+        <div id="content">
+            <?php
+                session_start();
+                if($_SESSION['username'] !== ""){
+                    $user = $_SESSION['username'];
+                    // afficher un message de bonne création du user
+                    echo "Utilisateur $user crée !";
+                }
+            ?>            
+        </div>
+    </body>
+</html>
