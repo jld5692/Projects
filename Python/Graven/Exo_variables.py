575e43a1d64702c0c0f9d63841195619e8b62bb8
jld5692 <jerome.ledorze@gmail.com>
2022-12-03 16:39:58 +0100

    Graven : conditions

--- Python/Graven/Exo_variables.py
+++ Python/Graven/Exo_variables.py
@@ -1,26 +1,13 @@
-def main():
-    # recolter une valeur de porte monnaie
-    # creer un produit qui aura pour vakeur 50
-    # afficher la n ouvelle valeur du porte monnaie, après son achat
+# code
+wallet = 5000
+computer_price = 6000
 
-    wallet = 200
-    productName = "chocolat"
-    productPrice = 50
-
-    print("Bienvenue à la chocolaterie de Suresnes")
-    nbBoite = int(input("Combien de boite souhaitez-vous acheter : "))
-
-    ticketPrice = nbBoite * productPrice
-
-    print("Pour " + str(nbBoite) + " vous nous devez " +
-          str(ticketPrice) + " euros")
-
-    print("--------------------------------------")
-    wallet = wallet - ticketPrice
-    print("il vous reste alors " + str(wallet) +
-          " euros dans votre porte monnaie.")
-    print("--------------------------------------")
-
-
-if __name__ == '__main__':
-    main()
+# le prix de l'ordinateur est inféreiru à 1000 euros
+if computer_price <= wallet:
+    print("Ready pour l'achat")
+    wallet -= computer_price
+    print("Il vous reste {} dans votre porte monnaie".format(wallet))
+else:
+    print("Pas assez d'argent sur le wallet")
+    manque = computer_price - wallet
+    print("Il vous manque {} euros dans votre wallet".format(manque))
