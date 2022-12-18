c8fbe27a50e57a10e791cb9fea5b518a2083fe8b
jld5692 <jerome.ledorze@gmail.com>
2022-12-18 20:38:43 +0100

    YT downloader GUI V2 - avec bouton, et progress bar

--- Utils/Youtube/youtubeDownloader_GUI.py
+++ Utils/Youtube/youtubeDownloader_GUI.py
@@ -9,18 +9,43 @@
 # ---------------------------------------------------------------------------------------------------
 
 from tkinter import *
+from tkinter import ttk
 from tkinter import filedialog
 from moviepy import *
 from moviepy.editor import VideoFileClip
 from pytube import YouTube
+from pytube import Playlist
 
 import shutil
 
+##########################################################################
 
-# Functions
+previousprogress = 0
+
+
+def on_progress(stream, chunk, bytes_remaining):
+    global previousprogress
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+
+    liveprogress = (int)(bytes_downloaded / total_size * 100)
+    if liveprogress > previousprogress:
+        previousprogress = liveprogress
+
+        progress_bar = ttk.Progressbar(screen, orient='horizontal',
+                                       mode='determinate', value=previousprogress, length=350)
+        canvas.create_window(250, 450, window=progress_bar)
+        value_label = ttk.Label(
+            screen, text="Progression: "+str(previousprogress)+"%")
+        canvas.create_window(250, 480, window=value_label)
+        canvas.update()
+##########################################################################
+
+
 def select_path():
-    # allows user to select a path from the explorer
+    # choix du chemin destination
     path = filedialog.askdirectory()
+    # on viens mettre à jour dans la fenêtre le chemin choisi
     path_label.config(text=path)
 
 
@@ -30,17 +55,33 @@ def download_file():
     # get selected path
     user_path = path_label.cget("text")
     screen.title('Downloading...')
-    # Download Video --> on pourrait améliorer le prog en laissant l'utilisateur choisir la qualité
-    mp4_video = YouTube(get_link).streams.get_highest_resolution().download()
-    vid_clip = VideoFileClip(mp4_video)
+
+    # On va afficher ici une barre de progression pour l'initialisation ( à voir si utile - premier refresh suffisant ?)
+    progress_bar = ttk.Progressbar(screen, orient='horizontal',
+                                   mode='determinate', length=350)
+    canvas.create_window(250, 450, window=progress_bar)
+
+    # permet de refresher la page et d'afficher du coup la progress bar.
+    canvas.update()
+
+#######################################################################################
+    mp4_video = YouTube(get_link)
+    mp4_video.register_on_progress_callback(on_progress)
+    vid = mp4_video.streams.get_highest_resolution().download()
+    # mp4_video.streams.filter(only_audio=True).first().download()
+    vid_clip = VideoFileClip(vid)
     vid_clip.close()
-    # move file to selected directory
-    shutil.move(mp4_video, user_path)
-    screen.title('Download Complete! Download Another File...')
+    # on recopie le fichier téléchargé dans le répertoire cible
+    shutil.move(vid, user_path)
+    screen.title('Téléchargement terminé ! Bordel ca marche !!!')
+#######################################################################################
 
 
+# Début du programme >>>
 screen = Tk()
-title = screen.title('Youtube Download')
+# titre de la fenêtre (en haut à gauche)
+title = screen.title('Youtube Downloader')
+# fenêtre qui hébergera l'ensemble
 canvas = Canvas(screen, width=500, height=500)
 canvas.pack()
 
@@ -50,27 +91,28 @@ logo_img = PhotoImage(file='C:\\Projects\\Utils\\Youtube\\yt.png')
 logo_img = logo_img.subsample(2, 2)
 canvas.create_image(250, 80, image=logo_img)
 
-# link field
+# ----------<
+# zone de saisie pour le lien YT
 link_field = Entry(screen, width=40, font=('Arial', 15))
 link_label = Label(screen, text="Lien YT à télécharger: ", font=('Arial', 15))
-
-# Select Path for saving the file
+# ----------<
+# Choix du chemin de sauvegarde
 path_label = Label(
     screen, text="Chemin cible du fichier télécharger", font=('Arial', 15))
 select_btn = Button(screen, text="Path", bg='red', padx='22',
                     pady='5', font=('Arial', 15), fg='#fff', command=select_path)
-# Add to window
+# ----------<
+# On ajoute à la fenêtre les éléments précédents
 canvas.create_window(250, 280, window=path_label)
 canvas.create_window(250, 330, window=select_btn)
-
-# Add widgets to window
 canvas.create_window(250, 170, window=link_label)
 canvas.create_window(250, 220, window=link_field)
-
-# Download btns
+# ----------<
+# Bouton de téléchargement
 download_btn = Button(screen, text="Download File", bg='green', padx='22',
                       pady='5', font=('Arial', 15), fg='#fff', command=download_file)
-# add to canvas
 canvas.create_window(250, 390, window=download_btn)
 
+# On lance l'affichage de la fenêtre avec tout ce qui a été préparé avant
+# Et attends ensuite l'intéraction avec l'utilisateur
 screen.mainloop()
